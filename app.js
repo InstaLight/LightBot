d4ffd582bb4a926bd24b5b9176d4a033222f0b7d
Patrick Smith <patchypants0307@icloud.com>
2017-08-30 23:44:50 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = require('./settings.json').token;
+const settings = require('./settings.json').token;
 const ddift = require('return-deep-diff');
+const chalk = require('chalk');
+require('./util/eventLoader')(client);
 
-client.on('ready',() => {
-	console.log('I\'m Online\nI\'m Online');
-});
 
-//guild events
-
-client.on('guildDelete', guild => {
-    console.log(`I have left ${guild.name} at ${new Date()}`);
-});
-
-client.on('guildCreate', guild => {
-    guild.defaultChannel.send(`I have joined ${guild.name}`);
-});
 
 client.on('guildMemeberAdd', guild => {
     guild.defaultChannel.send(`Welcome ${member.user.username} to the server!`);
@@ -29,52 +19,26 @@ client.on('guildMemberUpdate', (oMember, nMember)=> {
     console.log(ddift(oMember, nMember));
 })
 
-client.on('guildUpdate', (oGuild, nGuild)=> {
-    console.log(ddift(oGuild, nGuild));
-})
-
-//guild events
-
-//client events
-
-
-
-//client events
-
-//commands
-var prefix = "!"
-client.on('message', message => {
-	let args = message.content.split(' ').slice(1);
-	var result = args.join(' ');
-
-	if (!message.content.startsWith(prefix)) return;
-	if (message.author.bot) return;
-
-	if (message.content.startsWith(prefix + 'ping')) {
-		message.channel.sendMessage(`Pong! \`${Date.now() - message.createdTimestamp} ms\``);
-	} else
-
-	if (message.content.startsWith(prefix + 'send')) {
-		client.channels.get('245491978601627648').send('Hello from the bot testing channel!');
-	} else
-
-	if (message.content.startsWith(prefix + 'setgame')) {
-		if (!result) {
-			result = null;
-		}
-		client.user.setPresence(result);
-	} else
-
-	if (message.content.startsWith(prefix + 'setstatus')) {
-		if (!result) {
-			result = 'online';
-		}
-		client.user.setStatus(result);
-	} else
-
-	if (message.content.startsWith(prefix + 'foo')) {
-		message.channel.sendMessage('bar');
+var reload = (message, cmd) => {
+	delete require.cache[require.resolve('./commands/' + cmd)];
+	try {
+		let cmdFile = require('./commands/' + cmd);
+	} catch (err) {
+		message.channel.send(`Problem loading ${cmd}: ${err}`).then(
+		).catch(error => console.log(error.stack));
 	}
+	message.channel.send(`${cmd} reload was a success! :white_check_mark:`).then(
+	).catch(error => console.log(error.stack));
+};
+exports.reload = reload;
+
+ client.on('debug', e => {
+ });
+
+client.on('warn', e => {
 });
-//commands
-client.login(token);
+
+client.on('error', e => {
+});
+
+client.login(settings);
---
+++ events/ready.js
@@ -0,0 +1,4 @@
+const chalk = require('chalk');
+module.exports = client => {
+    console.log(chalk.bgYellow('I\'m Online\nI\'m Online'));
+}
